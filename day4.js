dbe4e5ede602724aa5bc827b6acae88f9d8f26ae
Thomas Harding <hardingt604@gmail.com>
2023-12-05 19:43:52 -0600

    day 4 part 2 works but this code is horrendous

--- day4.js
+++ day4.js
@@ -12,22 +12,30 @@ function syncReadFile(filename) {
 }
 
 let file = syncReadFile('./input4.txt');
-let pointTotal = 0;
+let numberOfScratchards = 0;
+let cardTotals = new Map();
+
+for (let i = 0; i < file.length; i++) {
+  cardTotals.set(i, 1); //start with 1 copy of each card
+}
 for (let i = 0; i < file.length; i++) { //for each line in the file
+  for (let n = 0; n < cardTotals.get(i); n++) { //n = the number of copies this scratchcard has
+    numberOfScratchards++;
     let numbers = (file[i].split(': ')[1].split(' | '));
     let winning = numbers[0].split(/\s+/);
     let owned = numbers[1].split(/\s+/);
-    // console.log('card numbers', winning, owned);
     let numMatches = 0;
     for (let j = 0; j < owned.length; j++) {
       if (winning.includes(owned[j]) && (owned[j] != '')) { //to account for single digit numbers
-        // console.log('matching on ', owned[j]);
         numMatches++;
       }
     }
-    if (numMatches >= 1) {
-      pointTotal += (1 * Math.pow(2, numMatches - 1));
+    for (let m = i; m < i+ numMatches; m++) { //go a numMatches amount of times
+      if (m+1 < file.length) {
+        cardTotals.set(m+1, (cardTotals.get(m+1) ?? 0) + 1)
+      }
     }
-    // console.log('card ', i, ': ', pointTotal, ' points');
+  }
 }
-console.log('final value: ', pointTotal);
+console.log('final map: ', cardTotals);
+console.log('final value: ', numberOfScratchards);
